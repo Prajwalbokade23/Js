94b6e78dc52d2c7bd83102ca51aec334dc82d174
Prajwalbokade23 <144212569+Prajwalbokade23@users.noreply.github.com>
2024-06-08 08:44:25 +0000

    01_variables learning

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144453;
+let accountEmail = "prajwal03gmail.com"
+var accountPassword = "123456"
+accountCity = "Nagpur"
+let accountState;
+
+
+// accountId = "8595859";'
+
+accountEmail="1234@gmail.com"
+accountPassword="797979"
+accountCity="Pune"
+
+console.table([accountEmail,accountId,accountPassword,accountCity, accountState]);
