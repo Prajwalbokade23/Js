89a06cd2b275198464ccb96fb527fa37cdd4a2ca
Prajwalbokade23 <144212569+Prajwalbokade23@users.noreply.github.com>
2024-06-08 09:55:36 +0000

    Data comparison Learn

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,67 @@
+// let score = "33abc";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// let score = null;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// let score = undefined;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// undefined = NaN (Not a Number) 
+
+// let isloggedIn = 1;
+
+// let booleanIsLoggedIn = Boolean(isloggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+// let isloggedIn = 0;
+
+// let booleanIsLoggedIn = Boolean(isloggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+//"" => false;
+// "abc"=> true;
+// 1 => true, 0 => false
+
+
+
+/* --------------Operation -----------*/
+
+// console.log(1+2+"2")
+// console.log("1"+2+2);
+
+let GameCounter = 100;
+console.log(++GameCounter);
+console.log(GameCounter++);
+
+//Please Refer MDN prefix and postfix operator working ;
+
+
+
