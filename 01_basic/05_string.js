bc8f1961ec9fb8084f3bd2713728c63b362804b3
Prajwalbokade23 <144212569+Prajwalbokade23@users.noreply.github.com>
2024-06-09 08:57:40 +0000

    String learn

---
+++ 01_basic/05_string.js
@@ -0,0 +1,12 @@
+let carname = "Pagani"
+let modelname = "2024"
+
+let string = `I have a ${carname} of year ${modelname}`;
+
+console.log(string);
+
+
+let state = new String ("Hello World");
+
+
+// For more string methods please console in browser
