22662677008d8249c68d1faabdaffdbd6481f5f8
Prajwalbokade23 <144212569+Prajwalbokade23@users.noreply.github.com>
2024-06-09 08:31:13 +0000

    Added Stack and Heap

--- 01_basic/DataTypes-Summary.js
+++ 01_basic/DataTypes-Summary.js
@@ -18,3 +18,33 @@ const myFunction = function (){
     console.log("Hello World")
 }
 console.log(typeof myFunction);
+
+
+
+/* ------------------------Memory------------------*/
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+
+let myname = "Prajwal"
+
+let anotherName = myname
+anotherName = "Rajesh"
+console.log(myname);
+console.log(anotherName);
+
+
+// Heap
+
+let userone = {
+    name: "userone",
+    email:"user@gmail.com"
+}
+
+let userTwo = userone
+
+userTwo.email = "prajwal@google.com"
+
+console.log(userone.email);
+console.log(userTwo.email);
+
