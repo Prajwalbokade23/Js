6fcc7c603fb7c21d37c27f522ddcaf55cc89aa48
Prajwalbokade23 <144212569+Prajwalbokade23@users.noreply.github.com>
2024-06-12 11:48:53 +0000

    Control statement

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,49 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3 
+
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+    default:
+        console.log("default catch");
+        break;
+}
+
+const week = "wed" 
+switch (week) {
+    case "mon":
+        console.log("monday");
+        break;
+    case "tue":
+        console.log("tuesday");
+        break;
+    case "wed":
+        console.log("wednesday");
+        break;
+    case "thru":
+        console.log("thrusday");
+        break;
+
+    default:
+        console.log("default catch");
+        break;
+}
