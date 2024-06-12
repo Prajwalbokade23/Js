f845078fe9c1163772e313c199141e1b4540a0c0
Prajwalbokade23 <144212569+Prajwalbokade23@users.noreply.github.com>
2024-06-12 07:40:24 +0000

    Scope level and hoisting

---
+++ 03_basic/02_scope.js
@@ -0,0 +1,43 @@
+function one () {
+    const username = "Prajwal"
+
+    function two (){
+        const website = "codewithpcb"
+        console.log(username)
+
+    }
+    // console.log(website);
+
+    // two()
+}
+
+// one()
+
+
+if (true) {
+    const username = "Prajwal"
+    if (username === "Prajwal"){
+        const website = " codwithpcb"
+        // console.log(username + website)
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+
+// ************ intresting ****************
+
+console.log(addone(5));
+
+function addone (num) {
+    return num+1
+}
+
+addtwo(5)
+
+const addtwo = function(num){
+    return num+2
+}
+
