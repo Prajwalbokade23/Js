6b7b74700869a355655f4bc2ab61aea76d63a66b
Prajwalbokade23 <144212569+Prajwalbokade23@users.noreply.github.com>
2024-06-13 10:00:25 +0000

    used reduce in shoppingcart

--- 05_iterations/nine.js
+++ 05_iterations/nine.js
@@ -5,4 +5,29 @@ const myNum = [1,2,3,4]
 //  return acc + currval
 // },0)
 
-console.log(mytotal);
+// console.log(mytotal);
+
+const shoppingCart = [
+    {
+        itemName : "js course",
+        price : 999
+    },
+    {
+        itemName : "python course",
+        price : 3999
+    },
+    {
+        itemName : "mobile dev",
+        price : 5999
+    },
+    {
+        itemName : "Data Science",
+        price : 15999
+    }
+]
+
+const pricetoPay = shoppingCart.reduce((acc,item)=>{
+    return acc + item.price 
+},0)
+
+console.log(pricetoPay);
